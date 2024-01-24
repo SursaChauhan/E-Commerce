d5d81f8ba8d58577594f4e91bb88503ff57c292c
SursaChauhan <ss0622187@gmail.com>
2024-01-25 01:05:43 +0530

    day-second

---
+++ my-project/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>
+
+            <div className="container"></div>
+            {/* logo and links  */}
+<div>
+    <a  href='#' className='text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl'>ESHOP</a>
+</div>
+            {/* navbar right section */}
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
